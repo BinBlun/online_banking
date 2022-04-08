2fe0fc82324adb50dbe6c622d5fd28cbc8ab90ed
Duc Anh <binflinterfler292001@gmail.com>
2022-04-09 00:23:10 +0700

    comment user controller

--- src/main/java/com/example/online_banking/controller/UserController.java
+++ src/main/java/com/example/online_banking/controller/UserController.java
@@ -1,23 +1,23 @@
-package com.example.online_banking.controller;
-
-import com.example.online_banking.model.User;
-import com.example.online_banking.service.RegisterService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.Valid;
-
-@Controller
-@RequestMapping("/user")
-public class UserController {
-
-    @Autowired
-    private RegisterService service;
-
+//package com.example.online_banking.controller;
+//
+//import com.example.online_banking.model.User;
+//import com.example.online_banking.service.RegisterService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import javax.validation.Valid;
+//
+//@Controller
+//@RequestMapping("/user")
+//public class UserController {
+//
+//    @Autowired
+//    private RegisterService service;
+//
 //    @RequestMapping(value = "")
 //    public String register(Model model){
 //        User register = new User();
@@ -39,4 +39,4 @@ public class UserController {
 //        service.save(register);
 //        return "redirect:/register";
 //    }
-}
+//}
