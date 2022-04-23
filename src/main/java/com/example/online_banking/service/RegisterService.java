13d0dfe01f48080bfe877e1ba929a7d99c2b49a8
chicutephomaique <1901040036@s.hanu.edu.vn>
2022-04-23 16:57:51 +0700

    chinh sua lai

--- src/main/java/com/example/online_banking/service/RegisterService.java
+++ src/main/java/com/example/online_banking/service/RegisterService.java
@@ -9,6 +9,7 @@ import com.example.online_banking.repository.CardRepository;
 import com.example.online_banking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -31,7 +32,7 @@ public class RegisterService {
 
         String accNum = randomNumber(9);
         do {
-            if (accountRepository.findByAccountNumber(accNum) != null) {
+            if (!CollectionUtils.isEmpty(accountRepository.findByAccountNumber(accNum))) {
                 accNum = randomNumber(9);
             } else {
                 break;
@@ -39,7 +40,7 @@ public class RegisterService {
         } while (true);
 //        // tao account
         Account account = new Account();
-        account.setAccountNumber(randomNumber(9));
+        account.setAccountNumber(accNum);
         account.setCurrentBalance(BigDecimal.valueOf(0));
         account.setUserId(customer);
 
