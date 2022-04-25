3e05b62b6ad6f1c3b3e799177bd9d6495b5dc4a9
TrungBH <trungbui1811@gmail.com>
2022-04-25 22:22:59 +0700

    update transfer

--- src/main/resources/db/migration/V1_1_1__init_data.sql
+++ src/main/resources/db/migration/V1_1_1__init_data.sql
@@ -9,3 +9,5 @@ insert into user_role(id, role_name, user_id) values(1, 'ROLE_ADMIN', 1);
 insert into user(id, email, username, password, encrypted_password, full_name, phone_number, ssn)
 values (2, 'user@demo.com', 'user', '123', '$2a$12$GhSRSIPa7.0BYqXDLS3u3OWTJeGChigXK2EEMPdIkawx7okI.cvmK', 'User Demo', '0852369741', '852369741');
 insert into user_role(id, role_name, user_id) values(2, 'ROLE_USER', 2);
+insert into account(account_id, account_number, bank_id, current_balance, user_id) values(1, '159736842', 1, 0, 2);
+insert into card(id, account_number, user_id) values(1, '159736842', 2);
