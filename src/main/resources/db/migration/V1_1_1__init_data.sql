6a9512c9026bafad069c95420e79db5a011d775e
Duc Anh <binflinterfler292001@gmail.com>
2022-04-30 21:37:28 +0700

    fix transactionSuccess

--- src/main/resources/db/migration/V1_1_1__init_data.sql
+++ src/main/resources/db/migration/V1_1_1__init_data.sql
@@ -11,3 +11,11 @@ values (2, 'user@demo.com', 'user', '123', '$2a$12$GhSRSIPa7.0BYqXDLS3u3OWTJeGCh
 insert into user_role(id, role_name, user_id) values(2, 'ROLE_USER', 2);
 insert into account(account_id, account_number, bank_id, current_balance, user_id) values(1, '159736842', 1, 0, 2);
 insert into card(id, account_number, user_id) values(1, '159736842', 2);
+
+insert into loans_package(loans_id, duration, interest_rate) values (1, 24, 0.02);
+insert into loans_package(loans_id, duration, interest_rate) values (2, 12, 0.01);
+insert into loans_package(loans_id, duration, interest_rate) values (3, 6, 0.0015);
+
+insert into saving_package(id, duration, interestRate) values (1, 24, 0.02);
+insert into saving_package(id, duration, interestRate) values (2, 12, 0.01);
+insert into saving_package(id, duration, interestRate) values (3, 6, 0.0015)
